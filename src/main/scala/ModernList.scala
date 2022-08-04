5107fe921a8a9f79970b1101674d35485f6567ec
DKotov <dkotov292@gmail.com>
2022-08-04 18:06:44 +0300

    6/6 and humiliation

--- src/main/scala/ModernList.scala
+++ src/main/scala/ModernList.scala
@@ -6,20 +6,38 @@ enum List[+A]:
   case Nil
   case Cons(now: A, other: List[A])
 
-//  @tailrec
-//  def allIndexes(xs: List[A], a: A): List[Int] =
-//    case List.Nil
-//    => new List[Int]
-//    case List.Cons(f, tl)
-//    => xs match
 
-//
-//  def allIndexesWhere(xs: List[A], pred: A => Boolean): List[Int] =
+def allIndexes[A](xs: List[A], a: A): List[Int] =
+
+  def F[A](x: List[A], b: A, buf: Int): List[Int] =
+    x match
+      case List.Nil => List.Nil
+      case List.Cons(now, other) =>
+        if (now == b)
+          List.Cons(buf, F(other, b, buf + 1))
+        else
+          F(other, b, buf + 1)
+
+  F(xs, a, 0)
+
+
+
+def allIndexesWhere[A](xs: List[A], pred: A => Boolean): List[Int] =
+  def F[A](x: List[A], pr: A => Boolean, buf: Int): List[Int] =
+    x match
+      case List.Nil => List.Nil
+      case List.Cons(now, other) =>
+        if (pr(now))
+          List.Cons(buf, F(other, pr, buf + 1))
+        else
+          F(other, pr, buf + 1)
+
+  F(xs, pred, 0)
 
 ////
 //@tailrec
 //def zip3[A, B, C](xs: List[A], ys: List[B], zs: List[C]): List[(A, B, C)] =
-//  //чесно кажучи це якась дупа
+//  //честно кажучи це якась дупа
 //  xs match
 //    case List.Nil =>
 //      ys match
@@ -54,21 +72,23 @@ def zip3[A, B, C](xs: List[A], ys: List[B], zs: List[C]): List[(A, B, C)] =
             case List.Cons(now2, other2) => List.Cons((now, now1, now2), zip3(other, other1, other2))
 
 
-
-//def unzip3(xs: List[(A, B, C)]): (List[A], List[B], List[C]) =
-
+def unzip3[A, B, C](xs: List[(A, B, C)]): (List[A], List[B], List[C]) =
+  xs match
+    case List.Nil => (List.Nil, List.Nil, List.Nil)
+    case List.Cons((a, b, c), other) =>
+      val (m, n, t) = unzip3(other)
+      (List.Cons(a, m), List.Cons(b, n), List.Cons(c, t))
 
 
 def toString(xs: List[Char]): String =
-    xs match
-      case List.Nil => ""
-      case List.Cons(now: Char, other: List[Char]) => "".+:(now).+(toString(other))
-
+  xs match
+    case List.Nil => ""
+    case List.Cons(now: Char, other: List[Char]) => "".+:(now).+(toString(other))
 
 def join(xs: List[String], separator: String): String =
-    xs match
-      case List.Nil => ""
-      case List.Cons(now, other) => now + separator + join(other, separator) // вже відчуваю сепаратор в самому кінці, що здаєця не треба...
+  xs match
+    case List.Nil => ""
+    case List.Cons(now, other) => now + separator + join(other, separator) // вже відчуваю сепаратор в самому кінці, що здаєця не треба...
 
 
 // NOTE: do not use this to demo the results.
---
+++ src/test/scala/ModernListSuite.scala
@@ -0,0 +1,40 @@
+package me.asdfa.postmortem.l1
+
+import munit.FunSuite
+
+class ModernListSuite extends FunSuite {
+  test("allIndexes of 1 on [1,2,3,4,1,5,6]") {
+    val expected = List.Cons(0, List.Cons(4, List.Nil))
+    val actual = allIndexes(List.Cons(1, List.Cons(2, List.Cons(3, List.Cons(4, List.Cons(1, List.Cons(5, List.Cons(6, List.Nil))))))), 1)
+    assertEquals(actual, expected)
+  }
+  test("allIndexes of >5 on [1,2,3,4,1,5,6]") {
+    val notexpected = List.Cons(5, List.Nil)
+    val expected = List.Cons(6, List.Nil)
+    val actual = allIndexesWhere(List.Cons(1, List.Cons(2, List.Cons(3, List.Cons(4, List.Cons(1, List.Cons(5, List.Cons(6, List.Nil))))))), x => x > 5)
+    assertNotEquals(actual, notexpected)
+    assertEquals(actual, allIndexesWhere(List.Cons(1, List.Cons(2, List.Cons(3, List.Cons(4, List.Cons(1, List.Cons(5, List.Cons(6, List.Nil))))))), x => x > 5))
+  }
+  test("zip3 on [1,2,3] and [4,5,6] and [7,8,9]") {
+    val expected = List.Cons((1, 4, 7), List.Cons((2, 5, 8), List.Cons((3, 6, 9), List.Nil)))
+    val actual = zip3(List.Cons(1, List.Cons(2, List.Cons(3, List.Nil))), List.Cons(4, List.Cons(5, List.Cons(6, List.Nil))), List.Cons(7, List.Cons(8, List.Cons(9, List.Nil))))
+    assertEquals(actual, expected)
+  }
+  test("unzip3 on [(1,4,7), (2,5,8), (3, 6, 9)]") {
+    val expected = (List.Cons(1, List.Cons(2, List.Cons(3, List.Nil))), List.Cons(4, List.Cons(5, List.Cons(6, List.Nil))), List.Cons(7, List.Cons(8, List.Cons(9, List.Nil))))
+    val actual = unzip3(List.Cons((1, 4, 7), List.Cons((2, 5, 8), List.Cons((3, 6, 9), List.Nil))))
+    assertEquals(actual, expected)
+  }
+  test("toString on [a,2,;]"){
+    val expected = "a2;"
+    val actual = me.asdfa.postmortem.l1.toString(List.Cons('a', List.Cons('2', List.Cons(';', List.Nil))))
+    assertEquals(actual, expected)
+  }
+  test("join on [\"1a\", \"vg\", \";eo\"], separator = \"0\""){
+    val expected = "1a0vg0;eo0"
+    val actual = join(List.Cons("1a", List.Cons("vg", List.Cons(";eo", List.Nil))), "0")
+    assertEquals(actual, expected)
+  }
+}
+
+//      join(xs: List[String], separator: String): String
